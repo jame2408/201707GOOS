5020ce1d698a65c092f9c44e0a87e7ce3846b16a
jame2408 <jame2408@gmail.com>
2017-07-01 14:17:14 +0800

    利用 EF，於 Controller 中撰寫 production code，綠燈。

--- GOOS_Sample/Controllers/BudgetController.cs
+++ GOOS_Sample/Controllers/BudgetController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GOOS_Sample.Models.DataModels;
 
 namespace GOOS_Sample.Controllers
 {
@@ -15,9 +16,16 @@ namespace GOOS_Sample.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult Add(BudgetAddViewModel model)
         {
+            using (var dbcontext = new GOOSEntities())
+            {
+                var budget = new Budget() { Amount = model.Amount, YearMonth = model.Month };
+                dbcontext.Budgets.Add(budget);
+                dbcontext.SaveChanges();
+            }
             ViewBag.Message = "added successfully";
             return View(model);
         }
