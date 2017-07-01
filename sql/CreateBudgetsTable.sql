7a8babc5a5767fe48eea9931da852bb7373956ca
jame2408 <jame2408@gmail.com>
2017-07-01 13:54:59 +0800

    簽入建立資料表 SQL

---
+++ sql/CreateBudgetsTable.sql
@@ -0,0 +1,8 @@
+﻿
+CREATE TABLE [dbo].[Budgets] (
+    [Amount]    DECIMAL (18) NOT NULL,
+    [YearMonth] VARCHAR (7)  NOT NULL,
+    CONSTRAINT [PK_Budgets] PRIMARY KEY CLUSTERED ([YearMonth] ASC)
+);
+
+select * from Budgets
