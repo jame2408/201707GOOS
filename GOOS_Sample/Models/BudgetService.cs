b1e3d45454a9440e200d873bd9c0dc96aa97fb8c
jame2408 <jame2408@gmail.com>
2017-07-01 17:12:47 +0800

    調整 BudgetService 以通過 isolated unit test for updated

--- GOOS_Sample/Models/BudgetService.cs
+++ GOOS_Sample/Models/BudgetService.cs
@@ -19,8 +19,26 @@ namespace GOOS_Sample.Models
         public void Create(BudgetAddViewModel model)
         {
 
-            var budget = new Budget() { Amount = model.Amount, YearMonth = model.Month };
-            this._budgetRepository.Save(budget);
+
+            //var budget = new Budget() { Amount = model.Amount, YearMonth = model.Month };
+            //this._budgetRepository.Save(budget);
+
+            var budget = this._budgetRepository.Read(x => x.YearMonth == model.Month);
+            if (budget == null)
+            {
+                this._budgetRepository.Save(new Budget() { Amount = model.Amount, YearMonth = model.Month });
+
+                var handler = this.Created;
+                handler?.Invoke(this, EventArgs.Empty);
+            }
+            else
+            {
+                budget.Amount = model.Amount;
+                this._budgetRepository.Save(budget);
+
+                var handler = this.Updated;
+                handler?.Invoke(this, EventArgs.Empty);
+            }
         }
     }
 }
